3285e2167f8bf82e4481acf88088231687249c06
Mehedi H Sajib <48852166+mehedihsajib@users.noreply.github.com>
2024-01-03 22:03:11 +0600

    daisy ui init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./assets/**/*.{js,ts,jsx,tsx}"],
+  plugins: [require("daisyui")],
+
   theme: {
     extend: {},
   },
-  plugins: [],
 };
